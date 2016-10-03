cf7cd3708ecec83202627f5bb43595991edd5b2b
Lorenzo <rellfysilveira@gmail.com>
2016-10-03 11:02:27 -0300

    Update

--- example/index.js
+++ example/index.js
@@ -13,13 +13,15 @@ app.use(express.static(path));
 console.log('Starting HTTP server on port 80');
 httpServer.listen(80);
 
+// Change the following details to match your Mongo DB info
 const DB_IP = '';
-const DB_PORT = '27017';
+const DB_PORT = '27017'; // the default port
 const DB_NAME = '';
 const DB_REQUIRE_LOGON = false;
 const DB_USER = '';
 const DB_PASS = '';
 
+// You don't need to change that ;)
 const mongoURL = (DB_REQUIRE_LOGON) ? ('mongodb://'+DB_USER+':'+DB_PASS+'@'+DB_IP+':'+DB_PORT+'/'+DB_NAME) : ('mongodb://'+DB_IP+':'+DB_PORT+'/'+DB_NAME);
 
 var cfg = {
@@ -34,23 +36,29 @@ var cfg = {
 nodePress.config(cfg).init(function(err){
 	if (err) { throw err }
 	console.log('NodePress initializated!');
-	
+
+	// Whenever the user/socket emits 'getPage'
 	nodePress.on('getPage', function(user, pg){
 		console.log('Requesting page  ' + pg);
 		var now = new Date().getTime();
+		// Call getPostsFromPage function
 		nodePress.getPostsFromPage(pg, function(data){
 			// data.last = true if the page the user is requesting is the last page (which cointains the earlier posts)
 			var after = new Date().getTime();
 			console.log('Request took ' + ((after - now)/1000) + 's');
+			// Send data to user
 			nodePress.socket.to(user).emit('postsFromPage', {"page":pg, "posts":data.posts, "last":data.last});
 		});
 	});
 
+	// Whenever the user/socket emits 'post'
 	nodePress.on('post', function(user, postData){
 		// Check if we have all the information needed
 		if (postData.title && postData.author && postData.body){
+			// Call post function
 			nodePress.post(postData, function(err){
 				if (err) { throw err };
+				// Send confirmation to user
 				nodePress.socket.to(user).emit('post-added');
 			});
 		} else {
